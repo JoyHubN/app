5a513852d629b8db911ed8f2aa363f7492d522d3
dev <n.cowaliow2017@yandex.ru>
2025-08-19 19:49:53 +0300

    Взаимодействие с бд

---
+++ models/User.js
@@ -0,0 +1,111 @@
+import bcrypt from 'bcrypt'
+import prisma from '../prisma.js'
+
+export async function registrationUser({fio, birthday, role, profiles: {email, password}}){ 
+    const hashedPassword = await bcrypt.hash(password, 10)
+  
+    try{
+        const user = await prisma.users.create({
+        data:{
+            fio: fio,
+            birthday: new Date(birthday),
+            status: true,
+            role: role,
+            profiles: {
+                create: {
+                  email: email,
+                  password: hashedPassword
+                },
+            },
+        },
+            include: { profiles: true}
+        });
+        return true;
+    }
+    catch (e) {
+      console.log(e);  
+      return e
+    }
+};
+
+
+export async function geUserForName(username){
+    const user = await prisma.users.findFirst({
+      where: {
+        profiles: {
+          some: {email: username}
+        }
+      },
+      include: {profiles: true}
+      
+    });
+    if (user == 'null'){
+      return false
+    }
+    else{
+      return user
+    }
+};
+
+
+export async function getUsers(){
+  const users = await prisma.users.findMany({ 
+    include: {
+      profiles: {
+        select: {
+          id: true,
+          user_id: true,
+          email: true
+        }
+      }
+    }
+  });
+  if (users){
+    return users
+  }
+  else{
+    return users.code
+  }
+}
+
+
+export async function getUser(user_id){
+  const users = await prisma.users.findUnique({ 
+    where: {user_id: user_id},
+      include: {
+        profiles: {
+          select: {
+            id: true,
+            user_id: true,
+            email: true
+          }
+        }
+      }
+  });
+  if (users){
+    return users
+  }
+  else{
+    return users.code
+  }
+}
+
+export async function blockUser(user_id, status) {
+  const user = await getUser(user_id);
+  if (user.code == null | typeof user.code == undefined){
+    const userUpdate = await prisma.users.update({
+      where:{ user_id: user_id},
+      data: {status},
+      include: {
+        profiles: {
+          select: {
+            user_id: true,
+            email: true, 
+          }
+        }
+      }
+    });
+    console.log(user)
+    return userUpdate;
+  }
+}
