afaa31223c0c78b65a28013ef076e09f5880d364
dev <n.cowaliow2017@yandex.ru>
2025-09-23 14:44:00 +0300

    getUser: приведение к числу, исправлено возвращение

--- routing/user/userRouter.js
+++ routing/user/userRouter.js
@@ -1,11 +1,13 @@
 import roleMiddleware from '../../middleware/roleMiddleware.js';
 import controller from '../authController.js'
 import Express from 'express';
+import { checkQuery, checkParams } from '../../validation/validated.js';
+import { userGetScheme, noParamsScheme } from '../../validation/schemes.js';
 
 const router = new Express();
 
-router.get('/user', roleMiddleware(['admin', 'user']), controller.getUser);
-router.get('/users', roleMiddleware(['admin']) ,controller.get_users);
+router.get('/user', roleMiddleware(['admin', 'user']), checkQuery(userGetScheme), controller.getUser);
+router.get('/users', roleMiddleware(['admin']), checkParams(noParamsScheme), controller.get_users);
 
 
 export default router
--- validation/validated.js
+++ validation/validated.js
@@ -9,3 +9,27 @@ export function checkBody(schema){
     }
 }
 
+
+export function checkQuery(schema){
+    return (req, res, next)=>{
+        const validationResult = schema.validate(req.query);
+
+        if(validationResult.error){
+            return res.status(400).send(validationResult.error.details)
+        }
+        next();
+    }
+}
+
+
+export function checkParams(schema){
+    return (req, res, next)=>{
+        const [query, body] = [req.query, req.body];
+        const validationResult = schema.validate({query, body});
+
+        if(validationResult.error){
+            return res.status(400).send(validationResult.error.details)
+        }
+        next();
+    }
+}
