c2d247252fd8468299e92a71f9a015b656368918
JoyHubN <n.cowalyow2017@yandex.ru>
2025-10-05 21:53:51 +0300

    добавлены расшифровки ошибок и валидация других полей

--- validation/schemes.js
+++ validation/schemes.js
@@ -1,17 +1,22 @@
 import BaseJoi from "joi";
-import joiDate from '@joi/date'
+import joiDate from '@joi/date';
 
 const Joi = BaseJoi.extend(joiDate);
 
 
 export const userScheme = Joi.object({
-    fio: Joi.string().required(),
+    fio: Joi.string().required()
+            .messages({
+                'any.required': 'Имя обязательно',
+                'string.empty': 'Имя не может быть пустым'
+            }),
     birthday: Joi.date()
             .format('YYYY.MM.DD').max(new Date().toISOString().replace('/T/',' ').split(' ')[0])
             .required()
             .messages({
                 'date.invalid': 'Дата рождения должна быть в формате YYYY.MM.DD',
-                'date.max': 'Дата рождения не может быть в будущем'
+                'date.max': 'Дата рождения не может быть в будущем',
+                'any.required': 'Дата рождения обязательна'
             }),
     password: Joi.string()
                 .required()
@@ -19,8 +24,15 @@ export const userScheme = Joi.object({
                     'string.min': 'Пароль должен содержать минимум 8 символов',
                     'string.pattern.base': 'Пароль должен содержать буквы разного регистра и цифры'
                 }),
-    email: Joi.string().required().email(),
-    role: Joi.string().valid('user', 'admin').required()
+    email: Joi.string().required().email()
+        .messages({
+            'any.required': 'Email обязателен',
+        }),
+    role: Joi.string().valid('user', 'admin').required()    
+        .messages({
+            'any.required': 'Роль обязательна',
+        })
+
 });
 
 
@@ -31,16 +43,20 @@ export const userGetScheme = Joi.object({
             'number.positive': 'ID должен быть положительным',
             'any.required': 'ID обязателен'
         })
-})
+});
 
 
-export const noParamsScheme = Joi.object({
-    query: Joi.object().max(0)
-        .messages({
-            'object.max': 'Query-параметры не допускаются',
-        }),
+export const paginationScheme = Joi.object({
+    query: Joi.object({
+        limit: Joi.number().integer().min(0).max(100).default(100),
+        offset: Joi.number().integer().min(0).default(0)
+    }),
     body: Joi.object().max(0)
         .messages({
             'object.max': 'Тело запроса не допускается',
         })
-})
+}).messages({
+    'number.integer': 'Offset и Limit должны быть целыми числами',
+    'number.min': '{#label} не может быть отрицательным или меньше 1',
+    'number.max': 'Limit не может превышать 100'
+});
